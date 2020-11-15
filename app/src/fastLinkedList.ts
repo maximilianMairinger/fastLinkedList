77cdab1af45a03f43097f1e168582cdb088b6abd
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2020-11-15 16:21:23 +0100

    iterator

--- app/src/fastLinkedList.ts
+++ app/src/fastLinkedList.ts
@@ -4,8 +4,10 @@ class Link<T> {
   }
 
   remove() {
-    this.val = this.next.val
-    this.next = this.next.next
+    const next = this.next
+    this.next = undefined
+    this.val = next.val
+    this.next = next.next
   }
 
   valueof() {
@@ -21,15 +23,20 @@ export class LinkedList<T> implements Iterable<T> {
   private head: Link<T>
   constructor(items: T[] | LinkedList<T> = []) {
     const preHead = {} as {next?: any}
+    let preCur: Link<T>
     let cur: Link<T> = preHead as any
     for (let item of items) {
+      preCur = cur
       cur.next = cur = new Link(item)
     }
     this.head = preHead.next
+    cur.next = preCur
   }
   
-  each(cb: (elem: T) => void) {
-    
+  each(cb: (el: T) => void) {
+    for (const el of this) {
+      cb(el)
+    }
   }
 
   protected get [0]() {
@@ -41,23 +48,32 @@ export class LinkedList<T> implements Iterable<T> {
 
   length() {
     let count = 0
-    this
+    for (const e of this) {
+      console.log(e)
+      count++
+    }
     return count
   }
 
   *[Symbol.iterator](): Iterator<T, any, unknown> {
-    let i = 0
-    while(i< 10) {
-      i++
-      yield i as any
+    if (this.head) {
+      let cur = this.head
+      yield cur.val
+      while(cur.next.next !== cur) {
+        cur = cur.next
+        yield cur.val
+      }
+      yield cur.next.val
     }
+    
   }
 }
 
 
 
 console.log("qwe")
-let e = new LinkedList([2, 3, 4, 5])
+//@ts-ignore
+let e = window.e = new LinkedList([2, 3, 4, 5])
 console.log(e)
 
 export default LinkedList
