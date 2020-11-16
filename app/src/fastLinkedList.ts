9c66e76f098345e205ec042bd3f60c8a1d5c37a8
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2020-11-16 14:49:35 +0100

    doubly linked & new Entry infrastructure & funcs

--- app/src/fastLinkedList.ts
+++ app/src/fastLinkedList.ts
@@ -1,92 +1,207 @@
+import { createBrotliCompress } from "zlib"
+
 class Link<T> {
-  constructor(public val: T, public next?: Link<T>) {
+  constructor(public value: T, public prev?: Link<T>, public next?: Link<T>) {
 
   }
+}
+
+class Entry<T> {
+  constructor(public readonly links: LinkedList<Link<T>>, protected readonly master: LinkedList<T>) {
+
+  }
+  move() {
+    return this
+  }
+
+
 
   remove() {
-    this.val = this.next.val
-    this.next = this.next.next
+    return this
   }
 
-  protected removeLast() {
-    this.next.next = undefined
-  }
-
-  valueof() {
-    return this.val
-  }
 }
 
 
- 
 
 
-export class LinkedList<T> implements Iterable<T> {
+
+export class LinkedList<T = unknown> implements Iterable<T> {
   private head: Link<T>
   private tail: Link<T>
-  constructor(items: T[] | LinkedList<T> = []) {
+  private _length: number
+  
+  constructor(item: LinkedList<T>)
+  constructor(item?: T, ...items: T[])
+  constructor(item: LinkedList<T> | T, ...items: T[]) {
     const preHead = this.tail = {} as any
-    this.bulkAdd(items)
+    this._bulkAdd(arguments as any)
     this.head = preHead.next
   }
+
+  values?(): Iterator<T, T, unknown>;
   
-  each(cb: (el: T) => void) {
+  each(cb: (el: T, i: number) => void) {
+    let i = 0
     for (const el of this) {
-      cb(el)
+      cb(el, i)
+      i++
     }
   }
 
-  bulkAdd(items: T[] | LinkedList<T> = []) {
-    let preCur: Link<T>
+  _bulkAdd(item: LinkedList<T>): void
+  _bulkAdd(item: T, ...items: T[]): void
+  _bulkAdd(item: LinkedList<T> | T, ...items: T[]): void {
+    const itms = item instanceof LinkedList ? item : arguments
     let cur: Link<T> = this.tail
-    delete cur.remove
-    for (let item of items) {
-      preCur = cur
-      cur.next = cur = new Link(item)
+
+    for (let item of itms) {
+      cur.next = cur = new Link(item, cur)
+    }
+
+    this._length += itms.length
+
+    this.tail = cur
+  }
+
+  bulkAdd(item: LinkedList<T>): Entry<T>
+  bulkAdd(item: T, ...items: T[]): Entry<T>
+  bulkAdd(item: LinkedList<T> | T, ...items: T[]): Entry<T> {
+    const links: LinkedList<Link<T>> = new LinkedList
+    const itms = item instanceof LinkedList ? item : arguments
+
+    let cur: Link<T> = this.tail
+
+    for (let item of itms) {
+      links.add(cur.next = cur = new Link(item, cur))
     }
     this.tail = cur
-    cur.remove = (cur as any).removeLast
-    cur.next = preCur
-  }
-  add(val: T) {
-    delete this.tail.remove
-    this.tail = this.tail.next = new Link(val, this.tail)
-    this.tail.remove = (this.tail as any).removeLast
-    return this.tail
-  }
+    this._length += itms.length
 
-  first() {
-    return this.head.val
+    return new Entry(links, this)
   }
-  last() {
-    return this.tail.val
-  }
-
-  length() {
-    let count = 0
-    for (const e of this) {
-      count++
+  call(...params: Parameters<T extends (...a: any) => void ? T : never>) {
+    for (let f of this) {
+      (f as any)(...params)
     }
-    return count
+  }
+  clear() {
+    this.tail = this.head = undefined
+    this._length = 0
+  }
+  hardClear() {
+    this.length = 0
+  }
+  pop() {
+
+  }
+  add(val: T): Entry<T> {
+    return new Entry(new LinkedList(this._add(val)), this)
+  }
+  _add(val: T): Link<T> {
+    this._length++
+    return this.tail = this.tail.next = new Link(val, this.tail)
+  }
+  dda(val: T): Entry<T> {
+    return new Entry(new LinkedList(this._dda(val)), this)
+  }
+  _dda(val: T): Link<T> {
+    this._length++
+    return this.head = this.head.prev = new Link(val, undefined, this.head)
+  }
+  eachRev(cb: (value: T, index: number) => void) {
+    if (this.tail) {
+      let i = 0
+      let cur = this.tail
+      while (cur.prev) {
+        cb(cur.value, i)
+        cur = cur.prev
+        i++
+      } 
+      cb(cur.value, i)
+    }
+  }
+  reverse() {
+    const n = new LinkedList()
+    for (let e of this) {
+      n._dda(e)
+    }
   }
 
-  *[Symbol.iterator](): Iterator<T, any, unknown> {
+  set first(to: T) {
+    this.head.value = to
+  }
+  set last(to: T) {
+    this.tail.value = to
+  }
+
+
+  get first() {
+    return this.head.value
+  }
+  get last() {
+    return this.tail.value
+  }
+
+  get length() {
+    return this._length
+  }
+  set length(to: number) {
+    this.length = Math.round(to)
+    
+    let i = 1
+
+    this.eachLink((link) => {
+      if (i >= to) {
+        link.prev = link.next = link.value = undefined
+      }
+      i++
+    })
+    
+    for (; i < to; i++) {
+      this.add(undefined)
+    }
+  }
+
+  map<NewT>(mapper: (val: T) => NewT): LinkedList<NewT> {
+    const n = new LinkedList(this)
+    n.eachLink((link) => {
+      link.value = mapper(link.value) as any
+    })
+    return n as any
+  }
+
+  private eachLink(cb: (link: Link<T>) => void) {
     if (this.head) {
       let cur = this.head
-      while(cur.next.next !== cur) {
+      while (cur.next) {
+        cb(cur)
         cur = cur.next
-        yield cur.val
-      }
-      yield cur.next.val
+      } 
+      cb(cur)
     }
-    
+  }
+
+
+
+  *[Symbol.iterator](): Iterator<T, T, any> {
+    if (this.head) {
+      let cur = this.head
+      while (cur.next) {
+        yield cur.value
+        cur = cur.next
+      } 
+      return cur.value
+    }
   }
 }
 
+
+LinkedList.prototype.values = LinkedList.prototype[Symbol.iterator]
 
 
 //@ts-ignore
-let e = window.e = new LinkedList([2, 3, 4, 5])
+let e = window.e = new LinkedList([2])
 console.log(e)
 
 export default LinkedList
