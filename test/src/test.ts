a2a3bf1a8c3620e06be9feb96f69c5a618d49433
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2020-11-17 19:42:37 +0100

    more test: toString & getter

--- test/src/test.ts
+++ test/src/test.ts
@@ -1,4 +1,9 @@
 import LinkedList from "../../app/src/fastLinkedList"
+import "./extend"
+
+
+
+
 
 describe("Core", () => {
   
@@ -23,13 +28,54 @@ describe("Core", () => {
     })
 
 
+    describe("getter", () => {
+
+      test("first", () => {
+        expect(ls.first).toBe("a")
+      })
+
+      test("last", () => {
+        expect(ls.last).toBe("d")
+      })
+
+      test("length", () => {
+        expect(ls.length).toBe(4)
+      })
+    })
+
+    describe("Iteration", () => {
+
+      
+      
+      
+      test("forof", () => {
+        const ex = expect(["a", "b", "c", "d"])
+        for (let e of ls) {
+          ex.ordered(e)
+        }
+      })
+
+      test("forof2", () => {
+        const ex = expect(["a", "b", "c", "d"])
+        for (let e of ls) {
+          ex.ordered(e)
+        }
+      })
+
+    })
+    
+
+
     describe("toString", () => {
-      test('Plain', () => {
+      test('plain', () => {
         let s = ls.toString()
         expect(s).toBe("[a, b, c, d]")
       })
+      test('json', () => {
+        let s = ls.toString(e => JSON.stringify(e))
+        expect(s).toBe('["a", "b", "c", "d"]')
+      })
     })
-  
     
   })
 
