3c6b03cbe1e07e892b575f7b5662038a7006a42d
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2020-11-17 19:41:51 +0100

    move extend to jest-expect-ordered

--- test/src/extend.ts
+++ test/src/extend.ts
@@ -1,68 +1,7 @@
-import keyIndex from "key-index"
+import expectOrdered from "jest-expect-ordered"
 
-declare global {
-  namespace jest {
-    interface Matchers<R, T> {
-      toBeOne(...expected: T[]): CustomMatcherResult
-      toBeOrderd(...expected: T[]): CustomMatcherResult
-    }
-  }
-}
-
-
-declare global {
-  namespace jest {
-    interface Matchers<R, T> {
-      toBeOne(...expected: T[]): CustomMatcherResult
-      once(got: T extends any[] ? T[number] : T): CustomMatcherResult
-      ordered(got: T extends any[] ? T[number] : T): CustomMatcherResult
-    }
-  }
-}
-
-
-const arrayCopy = keyIndex((ar: any[]) => {return {ar: [...ar], indicesUsed: []}})
-const sym = Symbol()
 expect.extend({
-  toBeOne(got, ...expected) {
-    return {
-      pass: got.includes(expected),
-      message: () => `Expected ${this.utils.printReceived(got)} to be one of ${this.utils.printExpected(expected)}`,
-    }
-  },
-  once(expected, got) {
-    const exp = arrayCopy(expected)
-    const ar = exp.ar
-
-    let ind = ar.indexOf(got)
-    const pass = ind !== -1
-    if (pass) {
-      exp.indicesUsed.push(ind)
-      ar[ind] = sym
-    }
-
-
-    return {
-      pass,
-      message: () => `Expected ${this.utils.printReceived(got)} to be tested once and out of [${expected.map((e, i) => exp.indicesUsed.includes(i) ? this.utils[expected[i] !== got ? "printExpected" : "printReceived"](e) : JSON.stringify(e)).join(", ")}].\nFaild at the ${exp.indicesUsed.length}. invocation.`,
-    }
-  },
-  ordered(expected, got) {
-    const exp = arrayCopy(expected)
-    const ar = exp.ar
-
-    let i = ar.indexOf(got)
-    const pass = i !== -1
-    if (pass) {
-      exp.indicesUsed.push(i)
-      ar.splice(i, 1)
-    }
-    return {
-      pass,
-      message: () => `Expected checks in the following succession [${expected.map((e, i) => i <= exp.indicesUsed.length ? this.utils[i < exp.indicesUsed.length ? "printExpected" : "printReceived"](e) : JSON.stringify(e)).join(", ")}].\nInsetead got ${this.utils.printReceived(got)} at the ${exp.indicesUsed.length+1}. invocation.`,
-    }
-  },
+  ...expectOrdered
+  // Own Extensions
+  
 })
-
-
-export {}
